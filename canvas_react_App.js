0d3157c752ad6dd0d002ebee428fe754c197cd4a
riela77 <143330718+riela77@users.noreply.github.com>
2025-05-21 17:44:52 +0900

    Add files via upload

---
+++ canvas_react_App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import CanvasBoard from './canvas/canvas';
+import CanvasBoard2 from './canvas/kimcanvas';
+
+function App() {
+  return (
+    <div className="App">
+      <CanvasBoard />
+      {/* <CanvasBoard2 /> */}
+    </div>
+  );
+}
+
+export default App;
